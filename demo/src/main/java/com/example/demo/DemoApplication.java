f76b622715869f25faf61cf4df7164941e0e22ed
softone <softone@DESKTOP-2RGE6HO>
2020-10-08 09:24:18 +0900

    초기생성

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	
+	private String ttt = "tttt";
+	
+	@RequestMapping("/")
+	 public String home() {
+	  return xxx();
+	 }
+
+	@RequestMapping("/index")
+	 public String index() {
+	  return "Hello Spring Boot index";
+	 }
+	
+	public String xxx() {
+		return "xxx";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
