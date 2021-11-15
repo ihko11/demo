b943f87b825cb816ef8fef77cf8e5a1b3eb80a45
ihko11 <ihko11@DESKTOP-R2BD4DV>
2021-11-15 09:32:54 +0900

    초기셋팅

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+        //이 부분 추가
+	@GetMapping("/thymeleaf")
+	public String thymeleaf() {
+		return "thymeleaf"; //thymeleaf.html과 매핑
+	}
+}
